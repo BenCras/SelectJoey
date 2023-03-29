be44ed727dd638a16237361eea050b84147d7e88
BenCras <r0879936@student.thomasmore.be>
2023-03-29 10:35:26 +0200

    push

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Select, SelectProvider } from '@mobile-reality/react-native-select-pro';
+
+const data = [
+  {
+    label: 'Option 1',
+    value: 'option1',
+  },
+  {
+    label: 'Option 2',
+    value: 'option2',
+  },
+  {
+    label: 'Option 3',
+    value: 'option3',
+  },
+  {
+    label: 'Option 4',
+    value: 'option4',
+  },
+];
+
+const MyView = () => {
+  return (
+      <View style={styles.container}>
+        <Select options={data} multiple={true} />
+      </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SelectProvider>
+        <MyView />
+      </SelectProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
